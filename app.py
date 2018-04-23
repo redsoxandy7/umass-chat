8947af2859fe85719e471897c4bb3c7b881f48ad
Andrew Reilly <andrewr71@live.com>
2018-04-22 21:27:21 -0400

    fixed dependency

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.exc import IntegrityError
+import psycopg2
 import os
 import config
 
